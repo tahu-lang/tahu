bd753dae94a138753303fdac54b105a6eb848709
Bulan Jatuh <lutfisobri@gmail.com>
2025-08-25 12:30:03 +0700

    feat: Implement parser functionality for class and function declarations, variable declarations, and expression parsing

---
+++ compiler/tahuc_parser/src/error.rs
@@ -0,0 +1,38 @@
+use tahuc_diagnostics::{diagnostic::Diagnostic, severity::Severity};
+use tahuc_span::Span;
+
+#[derive(Debug, Clone, PartialEq)]
+pub enum ParserError {
+    UnexpectedTopLevel {
+        found: String,
+        span: Span,
+    },
+
+    Expected {
+        expected: String,
+        found: String,
+        span: Span,
+    },
+
+    Unexpected {
+        unexcepted: String,
+        span: Span,
+    },
+}
+
+impl ParserError {
+    pub fn to_diagnostic(&self) -> Diagnostic {
+        match self {
+            ParserError::UnexpectedTopLevel {found, span} => {
+                let allowed = "Allowed declarations(class, interface, function) in top level scope";
+                Diagnostic::new(Severity::Error, format!("{} bot got {}", allowed, found)).with_span(*span)
+            }
+            ParserError::Expected {expected, found, span} => {
+                Diagnostic::new(Severity::Error, format!("{} {}", expected, found)).with_span(*span)
+            }
+            ParserError::Unexpected {unexcepted, span} => {
+                Diagnostic::new(Severity::Error, format!("Unexpected {}", unexcepted)).with_span(*span)
+            }
+        }
+    }
+}
---
+++ compiler/tahuc_parser/src/stmt.rs
@@ -0,0 +1,48 @@
+use tahuc_ast::nodes::{declarations::Visibility, statements::Statement};
+use tahuc_lexer::token::TokenKind;
+
+use crate::{error::ParserError, Parser};
+
+impl<'a> Parser<'a> {
+    pub(crate) fn statement(&mut self) -> Result<Statement, ParserError> {
+        match self.current_token().kind.clone() {
+            TokenKind::Val | TokenKind::Var => {
+                let variable = self.variable_declaration(Visibility::Private)?;
+                Ok(self.builder.variable_declaration_stmt(variable.span, self.file_id, variable))
+            },
+            TokenKind::Return => self.return_statement(),
+            _ => self.expression_statement(),
+        }
+    }
+
+    fn expression_statement(&mut self) -> Result<Statement, ParserError> {
+        let start_token = self.current_token().clone();
+        let expr = self.expression()?;
+
+        // Skip optional semicolon if present
+        if self.check(TokenKind::Semicolon) {
+            self.advance();
+        }
+
+        let end_token = self.current_token().clone();
+        let span = self.make_span(start_token, end_token);
+
+        Ok(self.builder.expression_statement(span, self.file_id, expr))
+    }
+
+    fn return_statement(&mut self) -> Result<Statement, ParserError> {
+        let start_token = self.current_token().clone();
+        self.advance();
+        let value = if !self.check(TokenKind::Semicolon) {
+            Some(self.expression()?)
+        } else {
+            None
+        };
+
+        let end_token = self.current_token().clone();
+        let span = self.make_span(start_token, end_token);
+
+        Ok(self.builder.return_statement(span, self.file_id, value))
+    }
+    // fn if_statement(&mut self) {}
+}
